e8e63e12957a055db0556545cb906120bf901b21
Igor Dias <igordiasth@gmail.com>
2020-04-14 17:30:34 +1200

    configure eslint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Typescript' })
-})
+app.get('/', (request, response) => response.json({ message: 'Typescript' }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
