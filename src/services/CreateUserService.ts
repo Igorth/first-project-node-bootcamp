ea5d614a510e2b3f798ab576cb69c0d753948e48
Igor Dias <igordiasth@gmail.com>
2020-04-24 16:43:20 +1200

    add Exception Handling, AppError

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import AppError from '../errors/AppError';
 import User from '../models/User';
 
 interface Request {
@@ -17,7 +18,7 @@ class CreateUserService {
     });
 
     if (checkUsersExists) {
-      throw new Error('Email address is already used');
+      throw new AppError('Email address is already used');
     }
 
     const hashedPassword = await hash(password, 8);
