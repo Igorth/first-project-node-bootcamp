d267ab6a4b94f87157b39daac584fbad29e0ae7e
Igor Dias <igordiasth@gmail.com>
2020-07-02 17:11:00 +1200

    configured amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@igordiasth.dev',
+      name: 'Igor do Razer Mustache',
+    },
+  },
+} as IMailConfig;
